32498a485e88d00f55f0e247b3bedc0143110ff3
Pratyush <sahupratyush1994@gmail.com>
2018-10-15 07:58:32 +0530

    First Git Commit

---
+++ src/test/java/com/bmw/bmw/AppTest.java
@@ -0,0 +1,24 @@
+package com.bmw.bmw;
+
+import static org.junit.Assert.assertTrue;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @SuppressWarnings("deprecation")
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+    	
+    	int a=10;
+    	Assert.assertEquals(10, a);
+    }
+}
